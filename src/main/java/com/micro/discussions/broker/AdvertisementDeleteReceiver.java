71dadc9fff198c7bc5f915d13d7890f3d712be88
ekaterina epishina laptop <katerinka_epishina@mail.ru>
2024-10-06 20:56:13 +0400

    брокер исправления

--- src/main/java/com/micro/discussions/broker/AdvertisementDeleteReceiver.java
+++ src/main/java/com/micro/discussions/broker/AdvertisementDeleteReceiver.java
@@ -1,19 +1,28 @@
 package com.micro.discussions.broker;
 
 import com.micro.discussions.services.DiscussionService;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.stereotype.Component;
 
-@Service
-public class AdvertisementDeleteReceiver {
+@Component
+public class AdvertisementDeleteReceiver implements ChannelAwareMessageListener {
     private final DiscussionService discussionService;
 
     public AdvertisementDeleteReceiver(DiscussionService discussionService) {
         this.discussionService = discussionService;
     }
 
-    @RabbitListener(queues = "advertisement")
-    public void consumeDeleteAdvertisement(String message) {
-        discussionService.deleteById(Integer.parseInt(message));
+    @Override
+    @RabbitListener(queues = "advertisement", ackMode = "MANUAL")
+    public void onMessage(Message message, Channel channel) throws Exception {
+        try {
+            discussionService.deleteById(Integer.parseInt(new String(message.getBody())));
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+        }
     }
 }
